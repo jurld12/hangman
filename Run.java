57fcdddec3eab20d4106b5e34a9e475f2c2f24bf
jurld12 <97524447+jurld12@users.noreply.github.com>
2024-03-30 18:14:07 +0400

    Comments update

--- Run.java
+++ Run.java
@@ -2,11 +2,18 @@
 public class Run {
 
 	public static void main(String[] args) {
+		//the creation of an instance of your main window 
+		MainWindow newgame = new MainWindow();
+		//definition of the title
+		//definition of the size
+		//visibility of this main window
 		
 		/*
 		 * Note: The provided Run class is 
 		 * the only place of the program that 
-		 * should have a main method. This
+		 * should have a main method.
+		 * 
+		 *  This
  		   class should not contain anything 
  		   else but the main method and the 
  		   main method should not contain any
