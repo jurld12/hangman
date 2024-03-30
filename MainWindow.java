2986f454f86acc920a015db8b129454eac940257
jurld12 <97524447+jurld12@users.noreply.github.com>
2024-03-30 18:36:36 +0400

    setResizable to false

--- MainWindow.java
+++ MainWindow.java
@@ -5,6 +5,7 @@ public class MainWindow extends JFrame {
 	//Constructor
 	MainWindow(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
 	}
 	//Show all components of the game
 	
