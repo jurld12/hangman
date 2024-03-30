89b451509e5366ff8374969927563c36b96c3d98
jurld12 <97524447+jurld12@users.noreply.github.com>
2024-03-30 18:06:55 +0400

    Update Run.java and add comments

--- Run.java
+++ Run.java
@@ -3,6 +3,17 @@ public class Run {
 
 	public static void main(String[] args) {
 		
+		/*
+		 * Note: The provided Run class is 
+		 * the only place of the program that 
+		 * should have a main method. This
+ 		   class should not contain anything 
+ 		   else but the main method and the 
+ 		   main method should not contain any
+ 		   code apart from the creation of an 
+ 		   instance of your main window and 
+ 		   the definition of the title, size, and
+ 		   visibility of this main window.*/
 
 	}
 
