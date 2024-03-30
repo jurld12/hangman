98665fe4ebef0fce4606972b48d72b2105cd8514
jurld12 <97524447+jurld12@users.noreply.github.com>
2024-03-30 23:56:24 +0400

    Label adding to the MainWindow and removeLife condition

--- MainWindow.java
+++ MainWindow.java
@@ -2,17 +2,27 @@ import javax.swing.*;
 
 public class MainWindow extends JFrame {
 
-	//Constuctor
-	public MainWindow(){
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(false);
+	public MainWindow() {
+		this.setLayout(null);
+		this.setSize(613,600);
+		this.setTitle("Hangman");
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(true);
+		//Guessing labels
 		
-		HealthPanel panel = new HealthPanel();
-		add(panel);
 		
 		
-		
+		HealthPanel UPanel = new HealthPanel();
+		this.add(UPanel.Label1());
+		this.add(UPanel.Label2());
+		this.add(UPanel.Label3());
+		this.add(UPanel.Label4());
+		this.add(UPanel.Label5());
+		this.add(UPanel.Label6());
+		this.add(UPanel.Label7());
+		if (UPanel.removeLife() <= 0)	{
+			//Subroutine for losing!
+		}
 	}
-	//Show all components of the game
-	
 }
