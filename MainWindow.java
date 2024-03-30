e53bd9c474e152a4a70f4c852bfc4dcaeeeaf973
jurld12 <97524447+jurld12@users.noreply.github.com>
2024-03-30 19:56:30 +0400

    Create an instance of HealthPanel to add to the MainWindow

--- MainWindow.java
+++ MainWindow.java
@@ -2,12 +2,17 @@ import javax.swing.*;
 
 public class MainWindow extends JFrame {
 
-	//Constructor
-	MainWindow(){
+	//Constuctor
+	public MainWindow(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
+		
+		HealthPanel panel = new HealthPanel();
+		add(panel);
+		
+		
+		
 	}
 	//Show all components of the game
 	
-	HealthPanel panel = new HealthPanel();
 }
