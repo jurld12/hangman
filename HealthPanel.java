cd23de0dadc309975363de2ea76c08c125caa9ff
jurld12 <97524447+jurld12@users.noreply.github.com>
2024-03-30 18:11:17 +0400

    Add removeLife method and return type to follow spec

--- HealthPanel.java
+++ HealthPanel.java
@@ -7,6 +7,21 @@ public class HealthPanel extends JPanel {
 	// remaining amount of incorrect guesses that
 	//can be made before the game is lost.
 	
-	
+	//Contain 7 labels 
+	//equal to the amount of incorrect guesses allowed.
+	//To begin with, the labels should be green.
 
+	public int removeLife(){
+		int guesses_total = 7;
+		int guesses_left = 7;
+		// called to update the element once an
+		//incorrect guess has been made.
+		//One of the green labels should be turned red. 
+		
+		// The return value
+		//of the method can indicate 
+		//if the player has run out of 
+		//their allotment of incorrect guesses.
+		return guesses_left;
+	}
 }
